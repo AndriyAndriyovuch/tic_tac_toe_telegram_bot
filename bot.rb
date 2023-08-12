e9a66283bc7dd51a5029f458eef0af2a61eb14cd
AndriiAndriyovuch <andriy.mazurok.tl@gmail.com>
2023-08-12 18:02:18 +0300

    minor fix

--- bot.rb
+++ bot.rb
@@ -12,8 +12,7 @@ Telegram::Bot::Client.run(token) do |bot|
   bot_value = ""
 
   bot.listen do |message|
-    my_message = message.text
-    my_message = "⬜️" if message.text.include?("⬜️")
+    my_message = message.text.include?("⬜️") ? "⬜️" : message.text
 
 
     case my_message
@@ -108,38 +107,10 @@ Telegram::Bot::Client.run(token) do |bot|
 
         bot.api.send_message(chat_id: message.chat.id, text: game.values.join(''),reply_markup: answers)
       end
-      
+
     when "⬜️"
       my_value = message.text[-3..-2].to_sym
       game[my_value] = user_value
-
-      # ============ New keyboard ============
-      new_keyboard = []
-      line_hash = []
-
-      game.each do |key, value|
-        if key[0] != "n"
-          line_hash << {text: "#{value} (#{key})"}
-        else
-          new_keyboard << line_hash
-          line_hash = []
-        end
-      end
-
-      new_keyboard << line_hash
-      # ======================================
-
-      # ========== Bot selects ===============
-      available = []
-
-      game.map do |key,value| 
-        if value == "⬜️"
-          available << key
-        end
-      end
-      
-      game[available[rand(available.length)]] = bot_value
-      # ======================================
 
       if game[:A1] == game[:A2] && game[:A1] == game[:A3] && game[:A3] != "⬜️" || 
         game[:B1] == game[:B2] && game[:B1] == game[:B3] && game[:B3] != "⬜️"  || # HORIZONTAL
@@ -153,14 +124,56 @@ Telegram::Bot::Client.run(token) do |bot|
         bot.api.send_message(chat_id: message.chat.id, text: game.values.join(''))
         bot.api.send_message(chat_id: message.chat.id, text: "Game Over")
       else
-        answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: new_keyboard, one_time_keyboard: true)
+        # ============ New keyboard ============
+        new_keyboard = []
+        line_hash = []
 
-        bot.api.send_message(chat_id: message.chat.id, text: "Select one:")
+        game.each do |key, value|
+          if key[0] != "n"
+            line_hash << {text: "#{value} (#{key})"}
+          else
+            new_keyboard << line_hash
+            line_hash = []
+          end
+        end
 
-        bot.api.send_message(chat_id: message.chat.id, text: game.values.join(''),reply_markup: answers)
+        new_keyboard << line_hash
+        # ======================================
+
+        # ========== Bot selects ===============
+        available = []
+
+        game.map do |key,value| 
+          if value == "⬜️"
+            available << key
+          end
+        end
+        
+        game[available[rand(available.length)]] = bot_value
+        if game[:A1] == game[:A2] && game[:A1] == game[:A3] && game[:A3] != "⬜️" || 
+          game[:B1] == game[:B2] && game[:B1] == game[:B3] && game[:B3] != "⬜️"  || # HORIZONTAL
+          game[:C1] == game[:C2] && game[:C1] == game[:C3] && game[:C3] != "⬜️"  || # HORIZONTAL
+          game[:A1] == game[:B1] && game[:A1] == game[:C1] && game[:C1] != "⬜️"  || # VERTICAL
+          game[:A2] == game[:B2] && game[:A2] == game[:C2] && game[:C2] != "⬜️"  || # VERTICAL
+          game[:A3] == game[:B3] && game[:A3] == game[:C3] && game[:C3] != "⬜️"  || # VERTICAL
+          game[:A1] == game[:B2] && game[:A1] == game[:C3] && game[:C3] != "⬜️"  || # DIAGONAL
+          game[:A3] == game[:B2] && game[:A3] == game[:C1] && game[:C1] != "⬜️"  # DIAGONAL
+  
+          bot.api.send_message(chat_id: message.chat.id, text: game.values.join(''))
+          bot.api.send_message(chat_id: message.chat.id, text: "Game Over")
+        else
+      # ======================================
+          answers = Telegram::Bot::Types::ReplyKeyboardMarkup.new(keyboard: new_keyboard, one_time_keyboard: true)
+
+          bot.api.send_message(chat_id: message.chat.id, text: "Select one:" )
+          bot.api.send_message(chat_id: message.chat.id, text: game.values.join(''),reply_markup: answers)
+        end
       end
     else
-      puts "IM ELSE"
+      p game
+      bot.api.send_message(chat_id: message.chat.id, text: "Your wrong" )
+
+      bot.api.send_message(chat_id: message.chat.id, text: game.values.join(''),reply_markup: answers)
     end
     
   end
